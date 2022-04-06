1d6e1e583de3dd382ef3be9a8d48b0a889a1d11e
Aleh Belski <aleh_belski@outlook.com>
2022-04-06 11:52:10 +0200

    refactor: changing base theme values to plain numbers

--- src/theme/base.ts
+++ src/theme/base.ts
@@ -1,29 +1,34 @@
 export const BaseTheme = {
   inputs: {
-    small: '40px',
-    medium: '50px',
-    large: '60px',
+    small: 40,
+    medium: 50,
+    large: 60,
   },
   checkInputs: {
     width: {
-      small: '40px',
-      medium: '50px',
-      large: '60px',
+      small: 40,
+      medium: 50,
+      large: 60,
     },
     height: {
-      small: '20px',
-      medium: '30px',
-      large: '40px',
+      small: 20,
+      medium: 30,
+      large: 40,
     }
   },
+  radioInputs: {
+    small: 15,
+    medium: 20,
+    large: 25,
+  },
   paddings: {
-    small: '5px',
-    medium: '10px',
-    large: '15px', 
+    small: 5,
+    medium: 10,
+    large: 15, 
   },
   fontSizes: {
-    small: '12px',
-    medium: '14px',
-    large: '16px', 
+    small: 12,
+    medium: 14,
+    large: 16, 
   }, 
 }
